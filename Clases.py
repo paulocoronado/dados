5d2c3be16af31d8c94bc39d33bd3132c48186ce6
Paulo César Coronado <paulocoronado@udistrital.edu.co>
2022-09-09 16:55:30 -0500

    Primer commit

---
+++ Clases.py
@@ -0,0 +1,43 @@
+import math
+
+class Calculadora:
+
+    def sumar(self, a,b):
+        return a+b    
+
+    def restar(self, a,b):
+        return a-b
+
+    def multiplicar(self, a,b):
+        return a*b
+
+    def dividir(self, a,b):
+        return a/b
+
+class CalculadoraTrigonometrica(Calculadora):
+
+    def seno(self,a):
+        return math.sin(a)
+
+    def coseno(self,a):
+        return math.cos(a)
+
+    def tangente(self,a):
+        return math.tan(a)
+
+class CalculadoraConversion(CalculadoraTrigonometrica):
+
+    def fahrenheit2celsius(self,a):
+        #Convertir grados fahrenheit a celsius
+        return (a-32)/1.8
+    
+    def pulgadas2centimetros(a):
+        return a*2.54
+
+
+
+
+
+
+
+
